3b0fc8e7f6376acd3d2acf53f94eaa5e087260b0
tropt1 <2717958@gmail.com>
2025-05-01 02:11:39 +0300

    refactor(server/deps): corrected app.py and added prometheus-client to requirements.txt

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
 from flask import Flask
+from prometheus_client import Counter, generate_latest
+
 app = Flask(__name__)
+REQUEST_COUNTER = Counter('http_requests_total', "Total HTPP Requests")
 
 @app.route('/<name>')
 def hello(name):
+    REQUEST_COUNTER.inc()
     return f"Hello, {name}\n"
+
+@app.route('/metrics')
+def metrics():
+    return generate_latest()
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080)
